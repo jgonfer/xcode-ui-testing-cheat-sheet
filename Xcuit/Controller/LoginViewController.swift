e72f19a6ef99218cc05447485db260754eaffb2f
Josep Gonzalez <josep.gonfer@gmail.com>
2016-07-10 18:03:53 +0200

    VC login class added. Screen edited.

---
+++ Xcuit/Controller/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  Xcuit
+//
+//  Created by jgonzalez on 8/7/16.
+//  Copyright © 2016 jgonfer. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
